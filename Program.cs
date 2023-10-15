8e23d2974c499d00908c84d17600984fe671a8f9
GcellyGomes <gicelly.gomes@etec.sp.gov.br>
2023-10-15 13:28:53 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Exercicios multiplos Gicelly-Gomes
+﻿int num, mult, resultado;
+
+
+Console.WriteLine("---MULTIPLO---");
+
+Console.WriteLine("Digite um número: ");
+num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Avaliar se é múltiplo de: ");
+mult = int.Parse(Console.ReadLine()!);
+
+resultado = num / mult;
+if (num % mult == 0)
+{
+    Console.WriteLine($"{num} é múltiplo de {mult}");
+}
+
+else 
+{
+    Console.WriteLine($"{num} não é múltiplo de {mult}");
+}
